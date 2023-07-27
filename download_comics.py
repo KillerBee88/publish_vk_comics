4d1a2745178e05aac54230356c5c6c7f1e05e8e2
KillerBee88 <hardmen08@gmail.com>
2023-07-27 20:57:42 +0300

    Create download_comics.py

---
+++ download_comics.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def download_comics(url, file_path):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
